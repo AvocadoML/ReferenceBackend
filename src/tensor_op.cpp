909d15b8dec464dd9159f037783041d329a5f2df
Maciej Kozarzewski <mk@eskrzynka.pl>
2022-03-10 01:48:29 +0100

    Added new method.

--- src/tensor_op.cpp
+++ src/tensor_op.cpp
@@ -114,6 +114,19 @@ namespace
 			dst[i] = src[i] + value;
 	}
 
+	template<typename T, typename U>
+	void kernel_add_tensors(T *dst, const T *src, U alpha, U beta, BroadcastedDimensions dims) noexcept
+	{
+		for (av_int64 i = 0; i < dims.first; i++)
+			for (av_int64 j = 0; j < dims.last; j++)
+			{
+				U tmp = alpha * static_cast<U>(src[i * dims.last + j]);
+				if (beta != zero<U>())
+					tmp += beta * dst[i * dims.last + j];
+				dst[i * dims.last + j] = tmp;
+			}
+	}
+
 	template<typename dstT, typename srcT, typename biasT>
 	void kernel_add_bias(dstT *dst, biasT alpha1, biasT alpha2, const srcT *src, const biasT *bias, biasT beta1, biasT beta2, biasT beta3,
 			const dstT *ext, BroadcastedDimensions dims, avActivationType_t type) noexcept
@@ -300,6 +313,54 @@ namespace avocado
 					break;
 				case AVOCADO_DTYPE_COMPLEX64:
 					kernel_add_scalar_to_tensor(getPointer<std::complex<double>>(cMem), getPointer<std::complex<double>>(aMem), scalar, elements);
+					break;
+				default:
+					return AVOCADO_STATUS_UNSUPPORTED_DATATYPE;
+			}
+			return AVOCADO_STATUS_SUCCESS;
+		}
+
+		avStatus_t refAddTensors(avContextDescriptor_t context, const void *alpha, const avTensorDescriptor_t aDesc, const avMemoryDescriptor_t aMem,
+				const void *beta, const avTensorDescriptor_t cDesc, avMemoryDescriptor_t cMem)
+		{
+			BroadcastedDimensions dimensions = getBroadcastDimensions(getTensor(aDesc), getTensor(cDesc));
+			switch (getTensor(cDesc).dtype())
+			{
+				case AVOCADO_DTYPE_UINT8:
+					kernel_add_tensors(getPointer<uint8_t>(cMem), getPointer<uint8_t>(aMem), getAlphaValue(alpha), getBetaValue(beta), dimensions);
+					break;
+				case AVOCADO_DTYPE_INT8:
+					kernel_add_tensors(getPointer<int8_t>(cMem), getPointer<int8_t>(aMem), getAlphaValue(alpha), getBetaValue(beta), dimensions);
+					break;
+				case AVOCADO_DTYPE_INT16:
+					kernel_add_tensors(getPointer<int16_t>(cMem), getPointer<int16_t>(aMem), getAlphaValue(alpha), getBetaValue(beta), dimensions);
+					break;
+				case AVOCADO_DTYPE_INT32:
+					kernel_add_tensors(getPointer<int32_t>(cMem), getPointer<int32_t>(aMem), getAlphaValue(alpha), getBetaValue(beta), dimensions);
+					break;
+				case AVOCADO_DTYPE_INT64:
+					kernel_add_tensors(getPointer<int64_t>(cMem), getPointer<int64_t>(aMem), getAlphaValue(alpha), getBetaValue(beta), dimensions);
+					break;
+				case AVOCADO_DTYPE_FLOAT16:
+					kernel_add_tensors(getPointer<float16>(cMem), getPointer<float16>(aMem), getAlphaValue(alpha), getBetaValue(beta), dimensions);
+					break;
+				case AVOCADO_DTYPE_BFLOAT16:
+					kernel_add_tensors(getPointer<bfloat16>(cMem), getPointer<bfloat16>(aMem), getAlphaValue(alpha), getBetaValue(beta), dimensions);
+					break;
+				case AVOCADO_DTYPE_FLOAT32:
+					kernel_add_tensors(getPointer<float>(cMem), getPointer<float>(aMem), getAlphaValue(alpha), getBetaValue(beta), dimensions);
+					break;
+				case AVOCADO_DTYPE_FLOAT64:
+					kernel_add_tensors(getPointer<double>(cMem), getPointer<double>(aMem), getAlphaValue<double>(alpha), getBetaValue<double>(beta),
+							dimensions);
+					break;
+				case AVOCADO_DTYPE_COMPLEX32:
+					kernel_add_tensors(getPointer<std::complex<float>>(cMem), getPointer<std::complex<float>>(aMem),
+							getAlphaValue<std::complex<float>>(alpha), getBetaValue<std::complex<float>>(beta), dimensions);
+					break;
+				case AVOCADO_DTYPE_COMPLEX64:
+					kernel_add_tensors(getPointer<std::complex<double>>(cMem), getPointer<std::complex<double>>(aMem),
+							getAlphaValue<std::complex<double>>(alpha), getBetaValue<std::complex<double>>(beta), dimensions);
 					break;
 				default:
 					return AVOCADO_STATUS_UNSUPPORTED_DATATYPE;
